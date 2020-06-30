26c88805ab5619cb78bc70628c44be158709762e
ShotaroKawano <shotaro.kawano009@gmail.com>
2020-06-30 17:17:25 +0900

    FIx: imgとjsのパス修正2

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -6,11 +6,11 @@
 <title>Example</title>
 <!-- Gambo -->
 <!-- Favicon Icon -->
-<link rel="icon" type="image/png" href="images/fav.png">
+<link rel="icon" type="image/png" href="{{ asset('/Frontend/images/fav.png') }}">
 
 <!-- Stylesheets -->
 <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-<link href={{ asset('/Frontend/vendor/unicons-2.0.1/css/unicons.css') }} rel='stylesheet'>
+<link href="{{ asset('/Frontend/vendor/unicons-2.0.1/css/unicons.css') }}" rel='stylesheet'>
 <link href="{{ asset('/Frontend/css/style.css') }}" rel="stylesheet">
 <link href="{{ asset('/Frontend/css/responsive.css') }}" rel="stylesheet">
 <link href="{{ asset('/Frontend/css/night-mode.css') }}" rel="stylesheet">
@@ -27,7 +27,7 @@
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+{{-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script> --}}
 </head>
 <body>
     <div class="container">
@@ -44,7 +44,7 @@
 	<script src="{{ asset('/Frontend/vendor/semantic/semantic.min.js') }}"></script>
 	<script src="{{ asset('/Frontend/js/jquery.countdown.min.js') }}"></script>
 	<script src="{{ asset('/Frontend/js/custom.js') }}"></script>
-	<script src="{{ asset('/Frontend/js/offset_overlay.js') }}j"></script>
+	<script src="{{ asset('/Frontend/js/offset_overlay.js') }}"></script>
 	<script src="{{ asset('/Frontend/js/night-mode.js') }}"></script>
 </body>
 </html>
