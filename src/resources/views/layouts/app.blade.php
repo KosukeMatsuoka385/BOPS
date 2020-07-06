29ed413355d45bb5df7389b9eca9482ef6993436
TatsuyaM <tat.mae084@gmail.com>
2020-07-07 03:04:03 +0900

    fix : cartの左からのスライド表示削除

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -229,115 +229,7 @@
                     </div>
                 </div>
                 <!-- Search Model End-->
-                <!-- Cart Sidebar Offset Start-->
-                <div class="bs-canvas bs-canvas-left position-fixed bg-cart h-100">
-                    <div class="bs-canvas-header side-cart-header p-3 ">
-                        <div class="d-inline-block  main-cart-title">カート <span>(合計アイテム数)</span></div>
-                        <button type="button" class="bs-canvas-close close" aria-label="Close"><i class="uil uil-multiply"></i></button>
-                    </div>
-                    <div class="bs-canvas-body">
-                        <div class="cart-top-total">
-                            <div class="cart-total-dil">
-                                <h4>BOPS Market</h4>
-                            <span> 合計金額の数値 </span>
-                            </div>
-                            {{-- <div class="cart-total-dil pt-2">
-                                <h4>Delivery Charges</h4>
-                                <span>$1</span>
-                            </div> --}}
-                        </div>
-                        <div class="side-cart-items">
-                            <div class="cart-item">
-                                <div class="cart-product-img">
-                                    <img src="{{ asset('/Frontend/images/product/img-1.jpg') }}" alt="">
-                                    <div class="offer-badge">6% OFF</div>
-                                </div>
-                                <div class="cart-text">
-                                    <h4>Product Title Here</h4>
-                                    <div class="cart-radio">
-                                        <ul class="kggrm-now">
-                                            <li>
-                                                <input type="radio" id="a1" name="cart1">
-                                                <label for="a1">0.50</label>
-                                            </li>
-                                            <li>
-                                                <input type="radio" id="a2" name="cart1">
-                                                <label for="a2">1kg</label>
-                                            </li>
-                                            <li>
-                                                <input type="radio" id="a3" name="cart1">
-                                                <label for="a3">2kg</label>
-                                            </li>
-                                            <li>
-                                                <input type="radio" id="a4" name="cart1">
-                                                <label for="a4">3kg</label>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <div class="qty-group">
-                                        <div class="quantity buttons_added">
-                                            <input type="button" value="-" class="minus minus-btn">
-                                            <input type="number" step="1" name="quantity" value="1" class="input-text qty text">
-                                            <input type="button" value="+" class="plus plus-btn">
-                                        </div>
-                                        <div class="cart-item-price">$10 <span>$15</span></div>
-                                    </div>
 
-                                    <button type="button" class="cart-close-btn"><i class="uil uil-multiply"></i></button>
-                                </div>
-                            </div>
-                            <div class="cart-item">
-                                <div class="cart-product-img">
-                                    <img src="{{ asset('/Frontend/images/product/img-2.jpg') }}" alt="">
-                                    <div class="offer-badge">6% OFF</div>
-                                </div>
-                                <div class="cart-text">
-                                    <h4>Product Title Here</h4>
-                                    <div class="cart-radio">
-                                        <ul class="kggrm-now">
-                                            <li>
-                                                <input type="radio" id="a5" name="cart2">
-                                                <label for="a5">0.50</label>
-                                            </li>
-                                            <li>
-                                                <input type="radio" id="a6" name="cart2">
-                                                <label for="a6">1kg</label>
-                                            </li>
-                                            <li>
-                                                <input type="radio" id="a7" name="cart2">
-                                                <label for="a7">2kg</label>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <div class="qty-group">
-                                        <div class="quantity buttons_added">
-                                            <input type="button" value="-" class="minus minus-btn">
-                                            <input type="number" step="1" name="quantity" value="1" class="input-text qty text">
-                                            <input type="button" value="+" class="plus plus-btn">
-                                        </div>
-                                        <div class="cart-item-price">$24 <span>$30</span></div>
-                                    </div>
-                                    <button type="button" class="cart-close-btn"><i class="uil uil-multiply"></i></button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="bs-canvas-footer">
-                        <div class="cart-total-dil saving-total ">
-                            <h4>Total Saving</h4>
-                            <span>$11</span>
-                        </div>
-                        <div class="main-total-cart">
-                            <h2>Total</h2>
-                            <span>$35</span>
-                        </div>
-                        <div class="checkout-cart">
-                            <a href="#" class="promo-code">Have a promocode?</a>
-                            <a href="#" class="cart-checkout-btn hover-btn">Proceed to Checkout</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- Cart Sidebar Offsetl End-->
                 <!-- Header Start -->
                 <header class="header clearfix">
 
@@ -516,7 +408,7 @@
                                 </div>
                             </nav>
                             <div class="header_cart order-1">
-                                <a href="#" id="cart_btn" class="cart__btn hover-btn pull-bs-canvas-left" title="Cart"><i class="uil uil-shopping-cart-alt"></i><span>Cart</span><i class="uil uil-angle-down"></i></a>
+                                <a href="#" id="cart_btn" class="cart__btn" title="Cart"><i class="uil uil-shopping-cart-alt"></i><span>Cart</span><i class="uil uil-angle-down"></i></a>
                             </div>
                             <div class="search__icon order-1">
                                 <a href="#" class="search__btn hover-btn" data-toggle="modal" data-target="#search_model" title="Search"><i class="uil uil-search"></i></a>
