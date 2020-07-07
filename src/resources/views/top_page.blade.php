ba57822b9d9091c6e089443efed1a7cc1d07b3a0
TatsuyaM <tat.mae084@gmail.com>
2020-07-07 15:21:52 +0900

    fix: topPageにlogo実装(gamboから変更)

---
+++ src/resources/views/top_page.blade.php
@@ -0,0 +1,10 @@
+﻿@extends("layouts.app")
+@section("content")
+
+	<!-- Body Start -->
+	<div class="wrapper" >
+        <img src="{{asset('/Frontend/images/BOPS.png')}}" alt="BOPS" style="width: 100%; padding-top:50px;">
+    </div>
+	<!-- Body End -->
+
+@endsection
